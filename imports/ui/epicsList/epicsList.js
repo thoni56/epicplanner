93a43193d8cf1b33b0040ce136c5112748b88c57
Thomas Nilefalk <thomas@nilefalk.se>
2018-10-28 16:40:54 +0100

    Move epic item in list to epicItem template

--- imports/ui/epicsList/epicsList.js
+++ imports/ui/epicsList/epicsList.js
@@ -1,8 +1,8 @@
 import { Template } from 'meteor/templating';
 import { Epics } from '../../api/epics';
-import '../epic/epic';
 
 import './epicsList.html';
+import './epicItem.html';
 
 Template.epicsList.helpers({
     epics() {
