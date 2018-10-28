8c86f8e867569bbfac31e5ad75fe724f9ece7afc
Thomas Nilefalk <thomas@nilefalk.se>
2018-10-28 17:04:31 +0100

    Iterate over all epic in preparation to draw them

--- imports/ui/capacityPlan/capacityPlan.js
+++ imports/ui/capacityPlan/capacityPlan.js
@@ -1,6 +1,7 @@
 import { Template } from 'meteor/templating';
 
 import './capacityPlan.html';
+import { Epics } from '../../api/epics';
 
 var ctx;
 const scale = 20;
@@ -9,6 +10,13 @@ Template.capacityPlan.onRendered(function () {
     const canvas = document.getElementById("planCanvas");
     ctx = canvas.getContext("2d");
     draw(20, 10, 150, 1);
+})
+
+Template.capacityPlan.helpers({
+    epics() { return Epics.find(); },
+    drawEpic() {
+        console.log(this);
+    }
 })
 
 function draw(x, y, effort, height) {
