b3c5f4db8981dd67d533e706f77fa3740de15729
Thomas Nilefalk <thomas@nilefalk.se>
2018-10-28 16:41:41 +0100

    Extract drawing of parameterized rect to separate function

--- imports/ui/capacityPlan/capacityPlan.js
+++ imports/ui/capacityPlan/capacityPlan.js
@@ -2,9 +2,17 @@ import { Template } from 'meteor/templating';
 
 import './capacityPlan.html';
 
-Template.capacityPlan.onRendered(function() {
+var ctx;
+const scale = 20;
+
+Template.capacityPlan.onRendered(function () {
     const canvas = document.getElementById("planCanvas");
-    const ctx = canvas.getContext("2d");
-    ctx.rect(20,20,150,150);
-    ctx.stroke();
+    ctx = canvas.getContext("2d");
+    draw(20, 10, 150, 1);
 })
+
+function draw(x, y, effort, height) {
+    ctx.fillStyle = "red";
+    ctx.fillRect(x, y, effort / height, height * scale);
+    ctx.stroke();
+}
